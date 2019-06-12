5df9a0b7786ff9d1d8ab1239f9df3b8be1f27ade
quxiao <quxiao@meitunmama.com>
2019-06-13 00:57:14 +0800

    学习了基本的Resource、ResourceLoader

--- src/main/java/xyz/quxiao/playwith/spring/XmlApplicationContextTest.java
+++ src/main/java/xyz/quxiao/playwith/spring/XmlApplicationContextTest.java
@@ -1,10 +1,11 @@
 package xyz.quxiao.playwith.spring;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
-import xyz.quxiao.playwith.spring.bean.Fruit;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 
 /**
  * @author 作者 :quxiao 创建时间：2019/6/11 20:06
@@ -12,14 +13,13 @@ import xyz.quxiao.playwith.spring.bean.Fruit;
 @Slf4j
 public class XmlApplicationContextTest {
 
-  public static void main(String[] args) {
-    DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory(null);
-    XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-    String path = "/ApplicationContext.xml";
-    ClassPathResource classPathResource = new ClassPathResource(path);
-    log.info("resource exist:{}", classPathResource.exists());
-    reader.loadBeanDefinitions(classPathResource);
-    Fruit fruit = beanFactory.getBean("fruit", Fruit.class);
-    log.info("fruit name:{}", fruit.getName());
+  public static void main(String[] args) throws IOException {
+    Resource classPathResource = new DefaultResourceLoader()
+        .getResource("classpath*:META-INF/MANIFEST.MF");
+    Scanner scanner = new Scanner(new BufferedInputStream(classPathResource.getInputStream()));
+    while (scanner.hasNextLine()) {
+      log.info("{}", scanner.nextLine());
+    }
+
   }
 }
